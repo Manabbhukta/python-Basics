fd5fd1bfd098aba584909e34ce13cb96b5133b17
manabbhukt <manabbhukta2007@gmail.com>
2026-08-06 20:57:27 +0530

    Create contact book with add and view functionality
    Implemented a basic contact book with options to add and view contacts.

---
+++ book.py
@@ -0,0 +1,24 @@
+contacts = {}
+
+def show_menu():
+    print("\n===== CONTACT BOOK =====")
+    print("1. Add New Contact")
+    print("2. View All Contacts")
+    print("3. Search Contact")
+    print("4. Delete Contact")
+    print("5. Exit")
+    print("========================")
+
+def add_contact():
+    name = input("Enter Name: ")
+    phone = input("Enter Phone Number: ")
+    contacts[name] = phone
+    print(f"'{name}' added successfully ✅")
+
+def view_contacts():
+    if len(contacts) == 0:
+        print("No contacts found")
+    else:
+        print("\nYour Contact List:")
+        for name, phone in contacts.items():
+            print(f"Name: {name} | Phone:
