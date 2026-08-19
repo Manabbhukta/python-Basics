dd368db63438890cb726b3ce03855c86837905e5
manabbhukt <manabbhukta2007@gmail.com>
2026-08-19 20:51:36 +0530

    Implement student grade calculator with percentage
    Added functionality to calculate and display the percentage of total marks.

--- grade calculator.py
+++ grade calculator.py
@@ -1,1 +1,18 @@
+# Student Grade Calculator
 
+name = input("Enter Student Name: ")
+
+marks = []  # Created a list
+subjects = ["Bangla", "English", "Math", "Physics", "Chemistry"]
+for sub in subjects:
+    m = int(input(f"Enter marks for {sub}: "))
+    marks.append(m)
+total = sum(marks)
+average = total / 5
+percentage = (total / 500) * 100  # Added percentage
+
+print("\n------ Result ------")
+print("Total Marks:", total)
+print("Average:", average)
+percentage = (total/500)*100
+print("Percentage:", percentage, "%")
